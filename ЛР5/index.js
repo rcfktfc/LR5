3dc8af342301d8ceb0dce4fbe8ecf3633b933f63
Максим-Савко <savkomax9@gmail.com>
2025-12-25 14:40:09 +0300

    Delete ЛР5 directory

--- ЛР5/index.js
+++
@@ -1,9 +0,0 @@
-import { Homework } from './homework.js';
-
-let hw = [
-    new Homework(1, "Математика", false, "Алгебра"),
-    new Homework(2, "Литература", true, "Книга")
-];
-
-hw[0].show();
-hw[1].show();
